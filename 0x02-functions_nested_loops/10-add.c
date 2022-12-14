65791a659606ef460fa303ca2887f14947b437e8
Chinstep <ifeanyichinmerem1994@gmail.com>
2022-12-14 15:08:46 -0800

    Tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+include "main"
+
+/**
+ * add - Entry point for the additio
+ * @: one value to be added together
+ * @b: second value to be added
+ * Return: Always 0 (Success)
+ **/
+
+int add(int a, int b)
+
+{
+	return (a + b);
+}
